6e6988c1633ab3f2a4998821ce57e9639168a218
mertceyhan <c.mertceyhan@hotmail.com>
2019-07-23 03:00:35 +0300

    Update for start listing messages

--- app/src/main/java/com/cafermertceyhan/smsuserconsentapi/MainActivity.kt
+++ app/src/main/java/com/cafermertceyhan/smsuserconsentapi/MainActivity.kt
@@ -1,12 +1,33 @@
 package com.cafermertceyhan.smsuserconsentapi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.phone.SmsRetriever
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        startSmsUserConsent()
+    }
+
+    private fun startSmsUserConsent() {
+        SmsRetriever.getClient(this).also {
+            //We can add user phone number or leave it blank
+            it.startSmsUserConsent(null)
+                    .addOnSuccessListener {
+                        Log.d(TAG, "LISTENING_SUCCESS")
+                    }
+                    .addOnFailureListener {
+                        Log.d(TAG, "LISTENING_FAILURE")
+                    }
+        }
+    }
+
+    companion object {
+        const val TAG = "SMS_USER_CONSENT"
     }
 }
