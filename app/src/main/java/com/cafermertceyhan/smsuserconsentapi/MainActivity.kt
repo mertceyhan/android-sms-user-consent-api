f568fcb258e8014b4959714c764a9cedf548858e
mertceyhan <c.mertceyhan@hotmail.com>
2019-07-23 03:31:20 +0300

    Add SmsBroadcastReceiver

--- app/src/main/java/com/cafermertceyhan/smsuserconsentapi/MainActivity.kt
+++ app/src/main/java/com/cafermertceyhan/smsuserconsentapi/MainActivity.kt
@@ -1,5 +1,7 @@
 package com.cafermertceyhan.smsuserconsentapi
 
+import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
@@ -7,11 +9,23 @@ import com.google.android.gms.auth.api.phone.SmsRetriever
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var smsBroadcastReceiver: SmsBroadcastReceiver
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         startSmsUserConsent()
+    }
+
+    override fun onStart() {
+        super.onStart()
+        registerToSmsBroadcastReceiver()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        unregisterReceiver(smsBroadcastReceiver)
     }
 
     private fun startSmsUserConsent() {
@@ -27,7 +41,25 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    private fun registerToSmsBroadcastReceiver() {
+        smsBroadcastReceiver = SmsBroadcastReceiver().also {
+            it.smsBroadcastReceiverListener = object : SmsBroadcastReceiver.SmsBroadcastReceiverListener {
+                override fun onSuccess(intent: Intent?) {
+                    intent?.let { context -> startActivityForResult(context, REQ_USER_CONSENT) }
+                }
+
+                override fun onFailure() {
+                }
+            }
+        }
+
+        val intentFilter = IntentFilter(SmsRetriever.SMS_RETRIEVED_ACTION)
+        registerReceiver(smsBroadcastReceiver, intentFilter)
+    }
+
     companion object {
         const val TAG = "SMS_USER_CONSENT"
+
+        const val REQ_USER_CONSENT = 100
     }
 }
